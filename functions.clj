5221c5c7ec73681a6a75e0737dba41f17d595257
Imre Bartis <imre.bartis@siili.com>
2017-10-22 17:40:55 +0300

    apply exercises added

--- functions.clj
+++ functions.clj
@@ -11,3 +11,39 @@
 
 (messenger "Hi sun!")
 ; => "Hi sun!"
+
+; variadic function (the param after '&' collects
+; all remaining args:
+
+(defn messenger [greeting & who]
+  (print greeting  who))
+
+(messenger "Hi" "you" "little")
+; => Hi (you little)
+
+; apply
+(let [a 1
+      b 2
+      more '(3 4)]
+  (apply print a b more))
+; => 1 2 3 4
+
+; & puts rest of args into a sequence
+(defn messenger [greeting & who]
+  ; apply gets rest of args out of the sequence
+  (apply print greeting who))
+
+(messenger "hi" "ho" "hee")
+; => hi ho hee
+
+(defn messenger2 [greeting & who]
+  ; apply gets rest of args out of the sequence
+  (apply print who))
+
+(messenger2 "hi" "ho" "hee")
+; => ho hee
+
+(let [numbers '(1 2 3)]
+     (apply + numbers))
+; => 6
+
