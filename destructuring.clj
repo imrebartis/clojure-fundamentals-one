96739b48ce613856a99db17366285da73c025660
Imre Bartis <imre.bartis@siili.com>
2017-10-27 13:46:49 +0300

    sequential destructuring

--- destructuring.clj
+++ destructuring.clj
@@ -12,3 +12,27 @@
 (let [[a & others] stuff]
   (println a) ; => 7
   (println others)) ; => (8 9 10 11)
+
+
+; sequential destructuring:
+
+(let [[_ & others] stuff] ; skip the first one
+  (println others)); => (8 9 10 11)
+
+
+(def names ["Bob" "Sally" "Joe"])
+(let [[first-name] names] first-name)
+; => "Bob"
+(let [[_ second-name] names] second-name)
+; => "Sally"
+(let [[_ & rest-names] names] rest-names)
+; => ("Sally" "Joe")
+
+
+(let [[_ & rest-names] names] _)
+; => "Bob"
+
+(defn rest_names [[_ & rest-names]]
+  rest-names)
+
+
