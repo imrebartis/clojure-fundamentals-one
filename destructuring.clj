cbee41b20ad801e873fbe768ea2d7740cbd7df2a
Imre Bartis <imre.bartis@siili.com>
2017-10-27 12:12:36 +0300

    destructuring part 2

--- destructuring.clj
+++ destructuring.clj
@@ -1,8 +1,14 @@
 (def stuff [7 8 9 10 11])
 
+(comment
 (let [[a b c] stuff]
   (list (+ a b) (+ b c)))
 ; => (15 17)
 (let [[a b c d e f] stuff]
   (list d e f))
 ; => (10 11 nil)
+)
+
+(let [[a & others] stuff]
+  (println a) ; => 7
+  (println others)) ; => (8 9 10 11)
