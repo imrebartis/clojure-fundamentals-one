249f1311c8d6ebe493c5a34e1df7f0b11a1643b8
Imre Bartis <imre.bartis@siili.com>
2017-10-30 09:35:59 +0200

    associative destructuring part 3

--- destructuring.clj
+++ destructuring.clj
@@ -53,3 +53,28 @@
         :or {c 3}} n]
   [a b c]) ; => [8 12 3]
 
+(defn game [planet & {:keys [human-players computer-players]}]
+  (println "Total players: " (+ human-players computer-players)))
+(game "Mars" :human-players 1 :computer-players 2)
+
+; => Total players:  3
+
+(defn draw-point [& {x :x y :y}]
+  [x y])
+(draw-point :x 10 :y :20) ; => [10 :20]
+
+(defn draw-point-2 [& {:keys [x y z]}]
+  [x y z])
+(draw-point-2 :x 10 :y :20) ; => [10 :20 nil]
+
+(defn draw-point-3 [& {:keys [x y z]
+                       :or {x 0
+                            y 0
+                            z 0}}]
+  [x y z])
+
+(draw-point-3 :x 10 :y :20) ; => [10 :20 0]
+
+(draw-point-3) ; => [0 0 0]
+
+
