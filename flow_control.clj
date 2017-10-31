a6b373afdfb7eab8afa40955dc076d566ae1e99e
Imre Bartis <imre.bartis@siili.com>
2017-10-31 14:53:50 +0200

    if/do added

--- flow_control.clj
+++ flow_control.clj
@@ -2,3 +2,20 @@
 (if [] :truthy :falsey) ; truthy
 (if nil :truthy :falsey) ; :falsey
 (if (seq []) :truthy :falsey) ; :falsey
+
+(str "2 is " (if (even? 2) "even" "odd"))
+; => "2 is even"
+
+; else-expression's optional:
+(if (true? false) "no way!")
+; => nil
+
+(if (even? 5)
+  (do (println "even")
+    true)
+  (do (println "odd")
+    false))
+; => false
+;; odd
+
+
